893c8837e7b44ad2a94ec9d81f97237e72540042
adamc66 <adam.cote66@gmail.com>
2019-07-09 13:33:22 -0400

    completed exercise 3

---
+++ exercise3.py
@@ -0,0 +1,27 @@
+# print("What is your name?")
+# user_name = input()
+# print("Hello, {}".format(user_name))
+
+# secret_password = "please"
+
+# print("What's the password?")
+# password_attempt = input()
+
+# correct_or_not = (password_attempt == secret_password)
+
+# print("That's {}!".format(correct_or_not))
+
+# print("How many cookies have been eaten?")
+# eaten = input()
+
+# # convert user input to integer and subtract it from 12
+# remaining_cookies = 12 - int(eaten)
+
+# print("There are {} cookies left.".format(remaining_cookies))
+
+#Your challenge: Let's try asking the user how old they are 
+#and have the program output what year they were born in.
+
+print("Hello user, how old are you?")
+user_age = input()
+print("you were born in {}".format(2019-int(user_age)))
