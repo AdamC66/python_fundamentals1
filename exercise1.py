ed8eda19c0ef16fcbc4f34192332c5f3710a92ec
adamc66 <adam.cote66@gmail.com>
2019-07-09 13:11:59 -0400

    completed exercise 1

---
+++ exercise1.py
@@ -0,0 +1,27 @@
+# print(2+3)
+# print(2)
+# print(3)
+
+# print('Hello world')
+# print("Hello world")
+
+# print("Betty's pie shop")
+# print('Betty\'s pie shop')
+
+# print("Hello\t\tworld")
+# print("1. Hello\n2. World")
+# print("Hello\n\n\n\n\nGoodbye world")
+# print("Hello \"world\"")
+# print("Hello \'world\'")
+
+# print("Ada Lovelace lived for {} years".format(1852 - 1815))
+
+print("Three times three is {}".format(3 * 3))
+
+name = "Betty"
+print("Hello {}".format(name))
+
+name1 = "Betty"
+name2 = "Bella"
+
+print("Hello {}, hello {}!".format(name1, name2))
